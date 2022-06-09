06cbdf3f14aa6cbd07b0373c76f152913dce5286
dany13gd <dany13gd@gmail.com>
2022-06-09 12:13:53 -0600

    add navigation complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  //viene de recipe.component.html un nivel arriba
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simple test', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540'),
-    new Recipe('A Test Recipe', 'This is a simple test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjxfLVMCkDKaGIpBm2C2HVLtg5LNVAsR4h9g&usqp=CAU')
+    new Recipe('A Fish Recipe', 'Fisrt Recipe', 'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540'),
+    new Recipe('A Pizza Recipe', 'Second Recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjxfLVMCkDKaGIpBm2C2HVLtg5LNVAsR4h9g&usqp=CAU')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
